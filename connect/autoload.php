82052b1b20aad9494bd98316140935936b01d977
Simon Skrødal <simon.skrodal@gmail.com>
2016-10-18 12:39:59 +1030

    Autoload

--- connect/autoload.php
+++ connect/autoload.php
@@ -11,10 +11,13 @@
 	);
 	// Add the paths to the class directories to the include path.
 	set_include_path(dirname(__DIR__) . PATH_SEPARATOR . implode(PATH_SEPARATOR, $paths));
-	error_log(dirname(__DIR__) . PATH_SEPARATOR . implode(PATH_SEPARATOR, $paths));
+/*
 	// Add the file extensions to the SPL.
 	spl_autoload_extensions(".class.php, .trait.php");
 	// Register the default autoloader implementation in the php engine.
 	spl_autoload_register();
 	//
+*/
+	spl_autoload_extensions(".php"); // comma-separated list
+	spl_autoload_register();
 	require_once('connect/config.php');
