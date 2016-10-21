d31a0b5363b2bef621312ed6acb08f3d897ef2ab
Simon Skrødal <simon.skrodal@gmail.com>
2016-10-21 13:03:26 +1030

    Edit response struct, add desc

--- connect/api/servicetrait.class.php
+++ connect/api/servicetrait.class.php
@@ -18,16 +18,18 @@
 		 * @return mixed
 		 */
 		public function serviceAccessDetails() {
-			$response = ['role' => 'Gjest', 'orgadmin' => false, 'superadmin' => false];
+			$response['access'] = ['orgadmin' => false, 'superadmin' => false, 'role' => 'Gjest', 'desc' => 'Begrenset tilgang til informasjon'];
 			$url = $this->dataporten->groupInvitationURL();
 			if($url !== false){
-				$response['orgadmin'] = true;
-				$response['group-invitation'] = $url;
-				$response['role'] = 'OrgAdmin';
+				$response['access']['orgadmin'] = true;
+				$response['access']['group-invitation'] = $url;
+				$response['access']['role'] = 'OrgAdmin';
+				$response['access']['desc'] = 'Tilgang til informasjon om din organisasjon og globale tall)';
 			}
 			if($this->dataporten->isSuperAdmin()){
-				$response['superadmin'] = true;
-				$response['role'] = 'SuperAdmin';
+				$response['access']['superadmin'] = true;
+				$response['access']['role'] = 'SuperAdmin';
+				$response['access']['desc'] = 'Sjef! Du har tilgang til alt :)';
 			}
 
 			return $response;
