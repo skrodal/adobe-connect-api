0175cf95207ff895865fe02f7dded474fd758e19
Simon Skrødal <simon.skrodal@gmail.com>
2016-10-24 11:01:03 +1030

    New route

--- connect/api/meetingstrait.class.php
+++ connect/api/meetingstrait.class.php
@@ -1,7 +1,6 @@
 <?php
 
 	namespace Connect\Api;
-	use Connect\Utils\Response;
 
 	/**
 	 * Note: In this API, we refer to rooms/meetings differently than Adobe Connect Web Services:
@@ -11,7 +10,6 @@
 	 * Meeting      |   Session
 	 * Room         |   Meeting
 	 */
-
 	trait MeetingsTrait {
 
 		/**
@@ -27,19 +25,35 @@
 		/**
 		 * NOTE: The call returns only users who logged in to the meeting as participants, not users who entered as guests.
 		 *
+		 * Large range will produce large amounts of data - handle with care...
+		 *
 		 * @param $start_timestamp
 		 * @param $end_timestamp
 		 *
 		 * @return array
 		 */
-		public function meetingsStatsInPeriod($start_timestamp, $end_timestamp) {
-			$range_start                              = date(DATE_ATOM, (int)$start_timestamp);
-			$range_end                                = date(DATE_ATOM, (int)$end_timestamp);
-			$response                                 = $this->callConnectApi(['action'                 => 'report-bulk-consolidated-transactions',
-			                                                                   'filter-type'            => 'meeting',
-			                                                                   'filter-gt-date-created' => $range_start,
-			                                                                   'filter-lt-date-created' => $range_end
-			]);
+		public function meetingsStatsInPeriod($start_timestamp, $end_timestamp, $org = null) {
+			// Convert timestamps for something eadible for Adobe Connect
+			$range_start = date(DATE_ATOM, (int)$start_timestamp);
+			$range_end   = date(DATE_ATOM, (int)$end_timestamp);
+			//
+			$request = ['action'                 => 'report-bulk-consolidated-transactions',
+			            'filter-type'            => 'meeting',
+			            'filter-gt-date-created' => $range_start,
+			            'filter-lt-date-created' => $range_end
+			];
+
+			if(!_is_null($org)){
+				if(!$this->dataporten->isSuperAdmin) {
+					// If not SuperAdmin, default back to logged in user's org
+					$org = $this->dataporten->userOrgId();
+				}
+				// Add org-filter
+				$request['filter-login-like'] = $org;
+			}
+
+			// Make the call
+			$response    = $this->callConnectApi($request);
 
 			$uniqueRoomAndUserCount['from_timestamp'] = (int)$start_timestamp;
 			$uniqueRoomAndUserCount['to_timestamp']   = (int)$end_timestamp;
@@ -56,36 +70,57 @@
 				if(isset($meeting->{'date-closed'})) {
 					//
 					$meetingDate = date("d.m.Y", strtotime((string)$meeting->{'date-closed'}));
-					if(!isset($roomAndUserCountByDate[$meetingDate])){
+					if(!isset($roomAndUserCountByDate[$meetingDate])) {
 						$roomAndUserCountByDate[$meetingDate]['duration_sec'] = 0;
-						$roomAndUserCountByDate[$meetingDate]['rooms'] = [];
-						$roomAndUserCountByDate[$meetingDate]['users'] = [];
-						// TODO: LEGG TIL EN ARRAY PER DATO MED TELLER FOR BRUKERE/ROM/SESJONER/VARIGHET
-
+						$roomAndUserCountByDate[$meetingDate]['sessions']     = 0;
+						$roomAndUserCountByDate[$meetingDate]['rooms']        = [];
+						$roomAndUserCountByDate[$meetingDate]['users']        = [];
 					}
+					// Keep track of sessions per date
+					$roomAndUserCountByDate[$meetingDate]['sessions']++;
 
-					// Add duration to total
+					// Duration of this session
 					$meetingDuration = abs(strtotime($meeting->{'date-closed'}) - strtotime($meeting->{'date-created'}));
+					// Add to total for this date
+					$roomAndUserCountByDate[$meetingDate]['duration_sec'] += $meetingDuration;
+					// ... and the total for period
 					$uniqueRoomAndUserCount['duration_sec'] += $meetingDuration;
-					// username
+
+					// User
 					$login = (string)$meeting->attributes()->{'principal-id'};
-					// room id
-					$room = (string)$meeting->attributes()->{'sco-id'};
-					// add sco as key (we use this to count total number of rooms involved in transactions)
-					$uniqueRoomAndUserCount['rooms'][$room] = true;
-					// add sco as key (we use this to count total number of users involved in transactions)
+					// Count users for this date
+					$roomAndUserCountByDate[$meetingDate]['users'][$login] = true;
+					// ...and the total for period
 					$uniqueRoomAndUserCount['users'][$login] = true;
+
+					// Room
+					$room = (string)$meeting->attributes()->{'sco-id'};
+					// Count rooms for this date
+					$roomAndUserCountByDate[$meetingDate]['rooms'][$room] = true;
+					// ...and the total for period
+					$uniqueRoomAndUserCount['rooms'][$room] = true;
+
 				}
 			}
 
+			// Ditch details, only want counts per date
+			foreach($roomAndUserCountByDate as $key => $dateObj) {
+				$roomAndUserCountByDate[$key]['rooms'] = count($roomAndUserCountByDate[$key]['rooms']);
+				$roomAndUserCountByDate[$key]['users'] = count($roomAndUserCountByDate[$key]['users']);
+			}
+
+
+			// Totals summary
 			$uniqueRoomAndUserCount['sessions']     = count($response->{'report-bulk-consolidated-transactions'}->row);
 			$uniqueRoomAndUserCount['rooms']        = count($uniqueRoomAndUserCount['rooms']);
 			$uniqueRoomAndUserCount['users']        = count($uniqueRoomAndUserCount['users']);
 			$uniqueRoomAndUserCount['duration_sec'] = $uniqueRoomAndUserCount['duration_sec'];
 
+			$response['requested_org'] = is_null($org) ? 'Alle' : $org;
 			$response['summary'] = $uniqueRoomAndUserCount;
+			$response['daily'] = $roomAndUserCountByDate;
 
-			return $uniqueRoomAndUserCount;
+			return $response;
 		}
 
 	}
